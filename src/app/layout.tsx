397bc0b6f3394ea2358ee4a6ad617d35959f1851
Hallotre <haywood.toby@gmail.com>
2025-10-27 03:30:44 +0100

    SKIBEN

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import MUIProvider from '@/providers/MUIProvider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Skiben',
+  title: 'SKIBEN',
   description: 'Skibens website',
   icons: {
     icon: '/favicon.ico',
